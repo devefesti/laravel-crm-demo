925ec270583cd62309f4e6a81a7c30832802590a
Ali Boudal <ali.boudal@efesti.com>
2024-05-10 12:21:30 +0200

    upgrade requests to https

--- packages/Webkul/Admin/src/Resources/views/layouts/master.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link rel="icon" href="https://www.efesti.com/wp-content/uploads/2022/07/cropped-fav.efesti.3.3-1-32x32.jpg">
